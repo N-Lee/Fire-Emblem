8705ed60aeb74b4a4fcd7b8b5d5fba3c0e29efd3
Nathan Lee <lee.nathan2@gmail.com>
2023-03-19 16:59:18 -0400

    Add mouse cursor

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CursorController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] Transform movePoint;
     [SerializeField] LayerMask collisionLayer;
+    [SerializeField] Grid grid;
+    [SerializeField] Tilemap cursorMap = null;
+    [SerializeField] Tilemap collisionMap = null;
+    [SerializeField] Tile cursorTile = null;
+    Vector3Int previousMousePos = new Vector3Int();
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +23,11 @@ public class CursorController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        MouseMove();
+    }
 
+    void ControllerMove()
+    {
         if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
         {
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
@@ -31,5 +40,24 @@ public class CursorController : MonoBehaviour
                 movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
             }
         }
+
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+    }
+
+    void MouseMove()
+    {
+        Vector3Int mousePos = GetMousePosition();
+        if (!mousePos.Equals(previousMousePos))
+        {
+            cursorMap.SetTile(previousMousePos, null);
+            cursorMap.SetTile(mousePos, cursorTile);
+            previousMousePos = mousePos;
+        }
+    }
+
+    Vector3Int GetMousePosition()
+    {
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        return grid.WorldToCell(mouseWorldPos);
     }
 }
